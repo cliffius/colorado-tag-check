349a750a721cd5f4771c9318946c18570d646e20
Cliff Cramer <cliffius@gmail.com>
2020-08-22 12:56:59 +0300

    fix out of range error

--- main.py
+++ main.py
@@ -99,19 +99,19 @@ def sendEmail(tags):
         var2 = 'those tags'
         
     else:
-        tagSubj = 'Colorado Tag ({1}) Available!'.format(tags[0])
+        tagSubj = 'Colorado Tag ({0}) Available!'.format(tags[0])
         var0 = 'tag is'
         var2 = 'that tag'
 
     tagMsg = '''        
-    The following {0} available:
+The following {0} available:
 
-    {1}
+{1}
 
-    Get {2}, bitch!
+Get {2}, bitch!
 
-    Love,
-    C
+Love,
+C
     '''.format(var0, '\n'.join(tags), var2)
 
     request_url = 'https://api.mailgun.net/v3/{0}/messages'.format(config.sandbox)
